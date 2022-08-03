4bc5ac1658757b72d2937a2c32a47ad12727c8df
yojohanshinwataikei <yojohanshinwataikei@yandex.com>
2022-08-03 10:29:43 +0800

    Overlap check for scenecontrol

--- server/src/checker/overlap.ts
+++ server/src/checker/overlap.ts
@@ -1,10 +1,10 @@
 import { DiagnosticSeverity, Location } from "vscode-languageserver"
 import { CstNodeLocation } from "chevrotain"
-import { AFFChecker, AFFError, AFFTrackItem, WithLocation, AFFTrackIdValue, AFFCameraEvent, AFFItem } from "../types"
+import { AFFChecker, AFFError, AFFTrackItem, WithLocation, AFFTrackIdValue, AFFCameraEvent, AFFItem, AFFSceneControlEvent, AFFFloat } from "../types"
 import { timings } from "../associated-data/timing"
 
 export const overlapChecker: AFFChecker = (file, error) => {
-	let trackRecord = new Map<AFFTrackIdValue, WithLocation<AFFTrackItem>[]>()
+	const trackRecord = new Map<AFFTrackIdValue, WithLocation<AFFTrackItem>[]>()
 	const checkItem = (item: WithLocation<AFFItem>) => {
 		if (item.data.kind === "arc") {
 			const arctaps = item.data.arctaps
@@ -37,7 +37,7 @@ export const overlapChecker: AFFChecker = (file, error) => {
 			}
 			trackRecord.get(trackId).push(item as WithLocation<AFFTrackItem>)
 		} else if (item.data.kind === "timinggroup") {
-			if(!timings.get(item.data).attributes.includes("noinput")){
+			if (!timings.get(item.data).attributes.includes("noinput")) {
 				for (const nestedItem of item.data.items.data) {
 					checkItem(nestedItem)
 				}
@@ -50,19 +50,48 @@ export const overlapChecker: AFFChecker = (file, error) => {
 	for (const items of trackRecord.values()) {
 		checkTrackOverlap(error, items)
 	}
-	const cameras:WithLocation<AFFCameraEvent>[]=[]
+	const cameras: WithLocation<AFFCameraEvent>[] = []
 	for (const item of file.items) {
-		if (item.data.kind === "camera"){
+		if (item.data.kind === "camera") {
 			cameras.push(item as WithLocation<AFFCameraEvent>)
-		}else if(item.data.kind === "timinggroup"){
+		} else if (item.data.kind === "timinggroup") {
 			for (const nestedItem of item.data.items.data) {
-				if (nestedItem.data.kind === "camera"){
+				if (nestedItem.data.kind === "camera") {
 					cameras.push(nestedItem as WithLocation<AFFCameraEvent>)
 				}
 			}
 		}
 	}
 	checkCameraOverlap(error, cameras)
+	const scenecontrolKinds = new Map<string, WithLocation<AFFSceneControlEvent>[]>()
+	const processScenecontrol = (scenecontrol: WithLocation<AFFSceneControlEvent>) => {
+		const kind = scenecontrol.data.sceneControlKind.data.value
+		if (["enwidencamera", "enwidenlanes"].includes(kind)) {
+			const values = scenecontrol.data.values;
+			if (values.data.length === 2) {
+				if (values.data[0].data.kind == "float" && values.data[1].data.kind == "int") {
+					if(!scenecontrolKinds.has(kind)){
+						scenecontrolKinds.set(kind,[])
+					}
+					scenecontrolKinds.get(kind).push(scenecontrol)
+				}
+			}
+		}
+	}
+	for (const item of file.items) {
+		if (item.data.kind === "scenecontrol") {
+			processScenecontrol(item as WithLocation<AFFSceneControlEvent>)
+		} else if (item.data.kind === "timinggroup") {
+			for (const nestedItem of item.data.items.data) {
+				if (nestedItem.data.kind === "scenecontrol") {
+					processScenecontrol(nestedItem as WithLocation<AFFSceneControlEvent>)
+				}
+			}
+		}
+	}
+	for (const [kind, scenecontrols] of scenecontrolKinds) {
+		checkScenecontrolOverlap(error, scenecontrols, kind)
+	}
 }
 
 const checkTrackOverlap = (error: AFFError[], items: WithLocation<AFFTrackItem>[]) => {
@@ -130,6 +159,34 @@ const checkCameraOverlap = (error: AFFError[], cameras: WithLocation<AFFCameraEv
 			report(item.location, lastLocation)
 		}
 		const end = start + item.data.duration.data.value
+		if (end > lastEnd) {
+			lastLocation = item.location
+			lastEnd = end
+		}
+	}
+}
+
+const checkScenecontrolOverlap = (error: AFFError[], scenecontrols: WithLocation<AFFSceneControlEvent>[], kind: string) => {
+	const report = (location: CstNodeLocation, lastLocation: CstNodeLocation) => {
+		error.push({
+			message: `The scenecontrol item with kind "${kind}" is overlapped with a previous scenecontrol item with kind "${kind}"`,
+			severity: DiagnosticSeverity.Warning,
+			location,
+			relatedInfo: [{
+				message: `The previous scenecontrol item`,
+				location: lastLocation
+			}]
+		})
+	}
+	const sortedByStart = scenecontrols.sort((a, b) => a.data.time.data.value - b.data.time.data.value)
+	let lastLocation: CstNodeLocation | null = null
+	let lastEnd: number = -Infinity
+	for (const item of sortedByStart) {
+		const start = item.data.time.data.value
+		if (start < lastEnd) {
+			report(item.location, lastLocation)
+		}
+		const end = start + (item.data.values.data[0] as WithLocation<AFFFloat>).data.value
 		if (end > lastEnd) {
 			lastLocation = item.location
 			lastEnd = end
